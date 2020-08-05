5fffe7d99afe4f4a1b5cd09f6020e851d80c2c3c
xj418974188 <xujuan418974188@163.com>
2020-08-05 21:47:30 +0800

    pytest 项目实战1

---
+++ pythoncode/calc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 计算器的代码
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
